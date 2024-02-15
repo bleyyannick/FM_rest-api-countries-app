b6c14b58bcfd7bfed92e0b4b40efaddfbe0fe1b0
Yannick Bley <bleyyannickpro@gmail.com>
2024-02-15 10:42:46 +0100

    feat: add style to main and cards and README.MD file changed

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,8 +1,25 @@
 /* eslint-disable react/prop-types */
 
+import styles from './CountryList.module.css';
+
 function CountryList({countries}) {
 
-  const countriesCard = countries.map((country)=> <div key={country.name.common}>{country.name.common}</div>)
+  const countriesCard = countries.map((country)=> {
+    return (
+      <div className={styles.card} key={country.name.common}>
+        <img src={country.flags.png} alt={country.flags.alt} />
+        
+        <div>
+        <h3>{country.name.common}</h3>
+          <ul>
+            <li><b>Population:</b><span>{country.population}</span></li>
+            <li><b>Region:</b><span>{country.region}</span></li>
+            <li><b>Capital:</b><span>{country.capital}</span></li>
+          </ul>
+        </div>
+      </div>
+    )
+  })
   return (
     <>
       {countriesCard}

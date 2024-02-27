0b9dfab7608bc08bd8baf8eaf42ccf67972ca8ad
Yannick Bley <bleyyannickpro@gmail.com>
2024-02-27 12:25:19 +0100

    feat: add link to come back at home page in country detail page

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
 import styles from './CountryPage.module.css'
 import Header from "../components/Header/Header";
 
@@ -11,7 +11,7 @@ function CountryPage() {
     useEffect(() => {
         const fetchCountry = async () => {
             try {
-               const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fulltext=true`)
+               const response = await fetch(`https://restcountries.com/v3.1/name/${name}`)
                if(!response.ok) throw new Error('Something went wrong with the request')
                const data = await response.json();
                  setCountryInfo(data);
@@ -31,37 +31,41 @@ function CountryPage() {
   return (
    <>
    <Header />
+   <div>
+      <Link to={`/`}>
+        Back
+      </Link>
+
+   </div>
     <section className={styles.countryDetails}>
-      <div className={styles.countryFlag}>
-      {countryInfo.map((country, key) => 
-        <img key={key} src={country.flags.png}  alt={country.flags.alt}  />
-      )}
-      </div>
-      {countryInfo.map((country, key) => 
-       <div key={key} className={styles.countryDescription}>
-          <div>
-            <h2>{country.name.common}</h2>
+    {countryInfo.map((country, key) =>
+      <article className={styles.countryContainer} key={key}>
+          <div className={styles.countryFlag}>
+              <img src={country.flags.png}  alt={country.flags.alt}  />
           </div>
-          <div className={styles.countryTextDetails}>
-            <ul className={styles.countryFirstList}>
-              <li>Native Name: {getNativeName(country.name.nativeName)} </li>
-              <li>Population:{country.population} </li>
-              <li>Region: {country.region} </li>
-              <li>Sub Region: {country.subregion}</li>
-              <li>Capital: {getCapitals(country.capital)}</li>
-            </ul>
-            <ul className={styles.countrySecondList}> 
-              <li>Top Level Domain: {country.tld[0]} </li>
-              <li>Currencies:{(Object.values(country.currencies)[0].name)} </li>
-              <li>Languages: {(Object.values(country.languages).map(languages => languages).join(','))} </li>
-            </ul>
+          <div className={styles.countryDescription}>
+              <div>
+                <h2>{country.name.common}</h2>
+              </div>
+              <div className={styles.countryTextDetails}>
+                <ul className={styles.countryFirstList}>
+                  <li>Native Name: {getNativeName(country.name.nativeName)} </li>
+                  <li>Population:{country.population} </li>
+                  <li>Region: {country.region} </li>
+                  <li>Sub Region: {country.subregion}</li>
+                  <li>Capital: {getCapitals(country.capital)}</li>
+                </ul>
+                <ul className={styles.countrySecondList}> 
+                  <li>Top Level Domain: {country.tld[0]} </li>
+                  <li>Currencies:{(Object.values(country.currencies)[0].name)} </li>
+                  <li>Languages: {(Object.values(country.languages).map(languages => languages).join(','))} </li>
+                </ul>
+              </div>
+              <div className={styles.countryBorders}>
+                <p>Border Countries: {country.borders?.map(border => border.toLowerCase()).join(',')}</p>
+              </div>
           </div>
-          <div className={styles.countryBorders}>
-            <p>Border Countries: {country.borders?.map(border => border.toLowerCase()).join(',')}</p>
-          </div>
-       </div>
-       )
-      }
+      </article>)}
     </section>
   </>
   )
